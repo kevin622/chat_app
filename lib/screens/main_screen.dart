8a43cc6f94f1227743a2bf09eaf68f74b4cbf2c2
박국현 <duru0622@gmail.com>
2023-08-03 20:00:15 +0900

    finished ui1

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -77,6 +77,7 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
               height: 280,
               width: MediaQuery.of(context).size.width - 40,
               margin: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(15),
@@ -85,6 +86,102 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                     color: Colors.black.withOpacity(0.3),
                     blurRadius: 15,
                     spreadRadius: 5,
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isSignupScreen = false;
+                          });
+                        },
+                        child: Column(
+                          children: [
+                            Text(
+                              "Login",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: !isSignupScreen
+                                    ? Palette.activeColor
+                                    : Palette.textColor1,
+                              ),
+                            ),
+                            if (!isSignupScreen)
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  top: 3,
+                                ),
+                                height: 2,
+                                width: 55,
+                                color: Colors.orange,
+                              )
+                          ],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isSignupScreen = true;
+                          });
+                        },
+                        child: Column(
+                          children: [
+                            Text(
+                              "Signup",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: isSignupScreen
+                                    ? Palette.activeColor
+                                    : Palette.textColor1,
+                              ),
+                            ),
+                            if (isSignupScreen)
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  top: 3,
+                                ),
+                                height: 2,
+                                width: 55,
+                                color: Colors.orange,
+                              )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    child: Form(
+                        child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.account_circle,
+                              color: Palette.iconColor,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Palette.textColor1),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(35),
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Palette.textColor1),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(35),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
                   ),
                 ],
               ),
